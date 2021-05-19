093829c7f8e6403ba05b1990ca3f79b17705c9b1
neil benadjaoud <neilbenadjaoud@neils-MacBook-Pro.local>
2021-05-19 13:52:15 -0700

    function with athlete heights and medals

---
+++ Athleteheightchart.R
@@ -0,0 +1,17 @@
+## File with a function that returns a boxplot chart of athlete's heights
+library(dplyr)
+library(ggplot2)
+library(forcats)
+
+athlete_df <- read.csv("Data/120 Years of Olympic History/athlete_events.csv")
+
+athlete_height <- function(dataset) {
+  dataset$Medal[is.na(dataset$Medal)] <- "Didn't Medal"
+  plot <- ggplot(data = dataset) +
+    geom_boxplot(mapping = aes(x = Medal, y = Height)) +
+    scale_x_discrete(limits = c("Gold", "Silver", "Bronze", "Didn't Medal"))
+    labs(title = "Heights and Medals of Olympic Athletes")
+  return(plot)
+}
+
+athlete_height(athlete_df)
