9139c4231600f21955ac01146b7d32745fe4b2ef
neil benadjaoud <neilbenadjaoud@neils-MacBook-Pro.local>
2021-05-19 14:58:01 -0700

    added chart  descirption to RMD file

--- Athleteheightchart.R
+++ Athleteheightchart.R
@@ -9,7 +9,7 @@ athlete_height <- function(dataset) {
   dataset$Medal[is.na(dataset$Medal)] <- "Didn't Medal"
   plot <- ggplot(data = dataset) +
     geom_boxplot(mapping = aes(x = Medal, y = Height)) +
-    scale_x_discrete(limits = c("Gold", "Silver", "Bronze", "Didn't Medal"))
+    scale_x_discrete(limits = c("Gold", "Silver", "Bronze", "Didn't Medal")) +
     labs(title = "Heights and Medals of Olympic Athletes")
   return(plot)
 }
